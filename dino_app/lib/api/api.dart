34c69429431382956f72a0ddaf83b6dcbdeacb4a
Oga29 <espagambetovoglan@mail.ru>
2024-01-08 20:28:13 +0100

    Changed api calling

--- dino_app/lib/api/api.dart
+++ dino_app/lib/api/api.dart
@@ -1,18 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:dino_app/models/dinosaur.dart';
 
 class DinosaurApi {
   final String apiUrl;
 
   DinosaurApi(this.apiUrl);
 
-  Future<List<String>> fetchDinosaurs() async {
+  Future<List<Dinosaur>> fetchDinosaursByEra(String era) async {
     try {
-      final response = await http.get(Uri.parse(apiUrl));
+      final response = await http.get(Uri.parse('$apiUrl?era=$era'));
 
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body);
-        return List<String>.from(data[0]);
+        return data.map((dinoData) => Dinosaur.fromJson(dinoData)).toList();
       } else {
         throw Exception('Failed to load dinosaurs. Status code: ${response.statusCode}');
       }
@@ -21,9 +22,18 @@ class DinosaurApi {
     }
   }
 
-  fetchDinosaurNames() {}
+  Future<Dinosaur> fetchDinosaur(String dinoName) async {
+    try {
+      final response = await http.get(Uri.parse('$apiUrl?name=$dinoName'));
 
-  fetchDinosaur(String dinoName) {}
-
-  fetchEra(String eraName) {}
+      if (response.statusCode == 200) {
+        final dynamic data = json.decode(response.body);
+        return Dinosaur.fromJson(data);
+      } else {
+        throw Exception('Failed to load dinosaur. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load dinosaur: $e');
+    }
+  }
 }
